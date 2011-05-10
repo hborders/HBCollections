aa025964c6aaa28c893309bddb181f74d7370c6a
hborders <hborders@mail.win.org>
2011-05-09 23:43:57 -0500

    Added unimplemented integration tests.

---
+++ NSEnumeratorHBCollectionsIntegrationTest.m
@@ -0,0 +1,65 @@
+#import "NSEnumerator+HBCollections.h"
+#import <GHUnit/GHUnit.h>
+#import <OCMock/OCMock.h>
+
+@interface NSEnumeratorHBCollectionsIntegrationTest : GHTestCase {
+
+}
+
+@end
+
+@implementation NSEnumeratorHBCollectionsIntegrationTest
+
+- (void) setUp {
+	[super setUp];
+}
+
+- (void) test_Map_Block_Not_Called_After_Succeeding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Map_Block_Not_Called_After_Preceding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Map_Block_Not_Called_After_Succeeding_Break_YES {
+	GHFail(@"Implement");
+}
+
+- (void) test_Map_Block_Not_Called_After_Preceding_Break_YES {
+	GHFail(@"Implement");
+}
+
+- (void) test_Filter_Block_Not_Called_After_Succeeding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Filter_Block_Not_Called_After_Preceding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Filter_Block_Not_Called_After_Succeeding_Break_YES {
+	GHFail(@"Implement");
+}
+
+- (void) test_Filter_Block_Not_Called_After_Preceding_Break_YES {
+	GHFail(@"Implement");
+}
+
+- (void) test_Break_Block_Not_Called_After_Succeeding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Break_Block_Not_Called_After_Preceding_Filter_NO {
+	GHFail(@"Implement");
+}
+
+- (void) test_Break_Block_Not_Called_After_Succeeding_Break_YES {
+	GHFail(@"Implement");
+}
+
+- (void) test_Break_Block_Not_Called_After_Preceding_Break_YES {
+	GHFail(@"Implement");
+}
+
+@end

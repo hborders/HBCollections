c182c5bd1008e00e51e8a7b4bd0eee08ddbb8bb2
hborders <hborders@mail.win.org>
2011-05-11 23:29:54 -0500

    Implemented integration tests.  Not yet passing. -Heath

--- NSEnumeratorHBCollectionsIntegrationTest.m
+++ NSEnumeratorHBCollectionsIntegrationTest.m
@@ -1,9 +1,18 @@
 #import "NSEnumerator+HBCollections.h"
+#import "HBCollectionsStackBufEnumerator.h"
 #import <GHUnit/GHUnit.h>
 #import <OCMock/OCMock.h>
 
 @interface NSEnumeratorHBCollectionsIntegrationTest : GHTestCase {
+	HBCollectionsStackBufEnumerator *testEnumerator;
 
+	NSUInteger mapCount;
+	NSUInteger filterCount;
+	NSUInteger breakCount;
+	
+	id (^mapBlock)(id);
+	BOOL (^filterBlock)(id);
+	BOOL (^breakBlock)(id);
 }
 
 @end
@@ -12,54 +21,102 @@
 
 - (void) setUp {
 	[super setUp];
-}
-
-- (void) test_Map_Block_Not_Called_After_Succeeding_Filter_NO {
-	GHFail(@"Implement");
+	
+	testEnumerator = [[[HBCollectionsStackBufEnumerator alloc] init] autorelease];
+	testEnumerator.elements = [NSArray arrayWithObjects:
+							   @"1",
+							   @"2",
+							   @"3",
+							   nil];
+	
+	mapCount = 0;
+	filterCount = 0;
+	breakCount = 0;
+	
+	mapBlock = [[^(id obj) {
+		mapCount++;
+		return obj;
+	} copy] autorelease];
+	filterBlock = [[^(id obj) {
+		filterCount++;
+		return YES;
+	} copy] autorelease];
+	breakBlock = [[^(id obj) {
+		breakCount++;
+		return NO;
+	} copy] autorelease];
 }
 
 - (void) test_Map_Block_Not_Called_After_Preceding_Filter_NO {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_filterEnumeratorUsingBlock:^(id obj) {
+		return NO;
+	}] hb_mapEnumeratorUsingBlock:mapBlock]);
+	
+	GHAssertEquals(mapCount, (NSUInteger) 0, nil);
 }
 
 - (void) test_Map_Block_Not_Called_After_Succeeding_Break_YES {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_mapEnumeratorUsingBlock:mapBlock] hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}]);
+	
+	GHAssertEquals(mapCount, (NSUInteger) 1, nil);
 }
 
 - (void) test_Map_Block_Not_Called_After_Preceding_Break_YES {
-	GHFail(@"Implement");
-}
-
-- (void) test_Filter_Block_Not_Called_After_Succeeding_Filter_NO {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}] hb_mapEnumeratorUsingBlock:mapBlock]);
+	
+	GHAssertEquals(mapCount, (NSUInteger) 0, nil);
 }
 
 - (void) test_Filter_Block_Not_Called_After_Preceding_Filter_NO {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_filterEnumeratorUsingBlock:^(id obj) {
+		return NO;
+	}] hb_filterEnumeratorUsingBlock:filterBlock]);
+	
+	GHAssertEquals(filterCount, (NSUInteger) 0, nil);
 }
 
 - (void) test_Filter_Block_Not_Called_After_Succeeding_Break_YES {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_filterEnumeratorUsingBlock:filterBlock] hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}]);
+	
+	GHAssertEquals(filterCount, (NSUInteger) 1, nil);
 }
 
 - (void) test_Filter_Block_Not_Called_After_Preceding_Break_YES {
-	GHFail(@"Implement");
-}
-
-- (void) test_Break_Block_Not_Called_After_Succeeding_Filter_NO {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}] hb_filterEnumeratorUsingBlock:filterBlock]);
+	
+	GHAssertEquals(filterCount, (NSUInteger) 0, nil);
 }
 
 - (void) test_Break_Block_Not_Called_After_Preceding_Filter_NO {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_filterEnumeratorUsingBlock:^(id obj) {
+		return NO;
+	}] hb_breakEnumeratorUsingBlock:breakBlock]);
+	
+	GHAssertEquals(breakCount, (NSUInteger) 0, nil);
 }
 
 - (void) test_Break_Block_Not_Called_After_Succeeding_Break_YES {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_breakEnumeratorUsingBlock:breakBlock] hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}]);
+	
+	GHAssertEquals(breakCount, (NSUInteger) 1, nil);
 }
 
 - (void) test_Break_Block_Not_Called_After_Preceding_Break_YES {
-	GHFail(@"Implement");
+	for (id obj in [[testEnumerator hb_breakEnumeratorUsingBlock:^(id obj) {
+		return YES;
+	}] hb_breakEnumeratorUsingBlock:breakBlock]);
+	
+	GHAssertEquals(breakCount, (NSUInteger) 0, nil);
 }
 
 @end
